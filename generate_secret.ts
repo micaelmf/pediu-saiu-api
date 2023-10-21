b24cd5df2377e7b0d37a8312da89be12b9b35416
Micael Ferreira <micaelmf@gmail.com>
2023-10-21 17:57:35 -0300

    chore: add function to gerenerate keys and remove migrations

---
+++ generate_secret.ts
@@ -0,0 +1,9 @@
+const bcrypt = require('bcrypt');
+
+const password = '-45)ETwdQ-Zt?2a8.oJ2';
+const saltRounds = 10000;
+
+bcrypt.hash(password, saltRounds, (err, hash) => {
+  if (err) throw err;
+  console.log(hash);
+});
